6e70deb874eac0890df0444683ed33f9e1f51d03
Kagami <79346269+Kagami011389@users.noreply.github.com>
2022-01-09 13:42:31 +0800

    C# version

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace hello_world
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(99, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "時間";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(232)))), ((int)(((byte)(142)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(36, 96);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(231, 200);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::hello_world.Properties.Resources.FGdUOGBUYAYMB_L;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(-12, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(321, 308);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox2.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.textBox2.Location = new System.Drawing.Point(381, 287);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(177, 25);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "直播時間 年月日時分秒";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.Maroon;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(381, 259);
+            this.button2.Margin = new System.Windows.Forms.Padding(8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "使用現在時間";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.Color.Maroon;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(319, 284);
+            this.button3.Margin = new System.Windows.Forms.Padding(8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(58, 28);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "更改";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(111, 305);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(74, 12);
+            this.button4.TabIndex = 6;
+            this.button4.Text = " ";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox3.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.textBox3.Location = new System.Drawing.Point(479, 223);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(79, 25);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "時間位移";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.BackColor = System.Drawing.Color.Maroon;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button5.Location = new System.Drawing.Point(410, 221);
+            this.button5.Margin = new System.Windows.Forms.Padding(8);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(58, 28);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "+";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(564, 319);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("微軟正黑體 Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "直播紀錄簿";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace hello_world
+{
+    public partial class Form1 : Form
+    {
+        private TimeStampProcess time = new TimeStampProcess();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            if(this.textBox1.Text == "")
+            {
+                MessageBox.Show("請更改直播時間");
+            }
+            else
+            {
+                this.textBox1.AppendText(Environment.NewLine);
+                this.textBox1.AppendText(time.GetTimeStamp(new DateTime(2021, 12, 19, 12, 00, 00)));
+                this.textBox1.Focus();
+            }
+            
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.textBox2.Text = $"{DateTime.Now.Year.ToString()}/" +
+                $"{DateTime.Now.Month.ToString()}/{DateTime.Now.Day.ToString()}" +
+                $" {DateTime.Now.Hour.ToString().PadLeft(2, '0')}:{DateTime.Now.Minute.ToString().PadLeft(2, '0')}" +
+                $":{DateTime.Now.Second.ToString().PadLeft(2,'0')}";
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            time.SetStreamDateTime(this.textBox2.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = $"{this.textBox2.Text.Replace('/', '_').Replace(':', '_')}.txt";
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.Write(this.textBox1.Text);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message+ "不可使用字元<（小於）>（大於）：（冒號）“（雙引號）/（正斜線）\\（反斜線）|（管線; pipe）？（問號）*（星號）");
+            }
+            
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string Text;
+            string[] textArray;
+            string[] messageArray;
+            string[] timeArray;
+            int shiftTime = 0;
+            
+            try
+            {
+                shiftTime = Convert.ToInt32(this.textBox3.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"時間位移錯誤:{ex.Message}");
+                //this.textBox1.AppendText(Environment.NewLine);
+                //this.textBox1.AppendText($"{ex.Message}");
+                return;
+            }
+            
+            Text = this.textBox1.Text;
+            textArray = Text.Split(new[] { "\r\n" }, StringSplitOptions.None);
+            timeArray = new string[textArray.Length];
+            messageArray = new string[textArray.Length];
+            this.textBox1.AppendText($"\r\n\r\ntime shift processing...\r\n");
+
+            for (int i=0;i<textArray.Length;i++)
+            {
+                try 
+                {
+                    timeArray[i] = textArray[i].Substring(0, 8);
+                    timeArray[i] = time.TimeShift(timeArray[i], shiftTime);
+                    messageArray[i] = textArray[i].Substring(8);
+                    this.textBox1.AppendText($"\r\n{timeArray[i]}{messageArray[i]}");
+                    
+                }
+                catch(Exception ex)
+                {
+                    if (textArray[i] != "time shift processing...")
+                    {
+                        this.textBox1.AppendText($"{textArray[i]}");
+                    }
+                    else
+                    {
+                        this.textBox1.AppendText(Environment.NewLine);
+                        this.textBox1.AppendText(Environment.NewLine);
+                        this.textBox1.AppendText($"time shift processing...");
+                        this.textBox1.AppendText(Environment.NewLine);
+                        continue;
+                    }
+
+                    
+                }
+            }
+        }
+    }
+    public class TimeStampProcess
+    {
+        private DateTime streamDateTime;
+        public String GetTimeStamp(DateTime time)
+        {
+            time = streamDateTime;
+            return DateTime.Now.Subtract(time).ToString().Substring(0, 8);
+        }
+
+        public void SetStreamDateTime(string times)
+        {
+            int[] time_int;
+            string[] time_string;
+            /*2021/12/19 13:32:05*/
+            char[] sep = { '/', ':' ,' '};
+            time_string = times.Split(sep,StringSplitOptions.RemoveEmptyEntries);
+  
+            
+            time_int = Array.ConvertAll(time_string,s => int.Parse(s)); 
+
+            streamDateTime = new DateTime(time_int[0], time_int[1], time_int[2], time_int[3], time_int[4], time_int[5]);
+        }
+
+        public string TimeShift(string time,int shiftSecond)
+        {
+            int hour, minute, second;
+            hour = Convert.ToInt32(time.Split(':')[0]);
+            minute = Convert.ToInt32(time.Split(':')[1]);
+            second = Convert.ToInt32(time.Split(':')[2]);
+            second += shiftSecond;
+            if(second >= 60)
+            {
+                second -= 60;
+                minute++;
+            }
+            if(second < 0)
+            {
+                minute--;
+                second += 60;
+            }
+            if(minute >= 60)
+            {
+                minute -= 60;
+                hour++;
+            }
+            if (minute < 0)
+            {
+                minute += 60;
+                hour--;
+            }
+            time = $"{Convert.ToString(hour).PadLeft(2,'0')}:{Convert.ToString(minute).PadLeft(2, '0')}:{Convert.ToString(second).PadLeft(2,'0')}";
+            return time;
+        }
+
+       
+    }
+}
